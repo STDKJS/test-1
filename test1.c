c587792f48de7bacd967cf49aba2723801088abd
Jusung Kim <jusung10191@gmail.com>
2023-10-28 00:10:33 +0900

    second commit

--- test1.c
+++ test1.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 void main()
 {
-    printf("10/27 github에 저장하기.");
+    printf("1. git init -> 처음에 무조건 해야함");
+    printf("2. git add . -> (.)은 파일을 전부 올린다는 뜻(ex.git add index.html)도 가능");
+    printf("3. git status -> 상태 알려줌");
+    printf("4. git commit -m "first commit" -> commit은 히스토리 알려줌");
 }
